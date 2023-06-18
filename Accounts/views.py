8af4350a0a5cf8848be1d803217da17bd690deb8
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-18 15:25:16 +0200

    create RegisterView in Accounts.views for Get & Post Request

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework import  status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import  permission_classes
+
+@permission_classes([AllowAny,])
+class RegisterView(APIView):
+    def get(self, request):
+        admins = NewUser.objects.all()
+        data = UserSerializer(admins, many=True).data
+        return Response(data)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({
+                "user": serializer.data,
+                #"token": serializer.token
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
