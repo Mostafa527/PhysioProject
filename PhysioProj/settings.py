bee10c1a27a6e97858f1b5882776e703c7e1666b
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-18 14:21:22 +0200

    AUTH_USER_MODEL

--- PhysioProj/settings.py
+++ PhysioProj/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Account'
+    'Accounts'
 ]
 
 MIDDLEWARE = [
@@ -80,6 +80,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+AUTH_USER_MODEL = 'Account.NewUser'
 
 
 # Password validation
