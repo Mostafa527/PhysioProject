87b849c24ee8f43e6f6499f11d37a5c029865a57
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-18 14:26:37 +0200

    Add postgresql database & update installed app with built in apps

--- PhysioProj/settings.py
+++ PhysioProj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'rest_framework.authtoken',
     'Accounts'
 ]
 
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'PhysioProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Final_Physio_DB',
+        'USER': 'postgres',
+        'PASSWORD': '42105518',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
