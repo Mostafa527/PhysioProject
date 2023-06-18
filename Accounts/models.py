c92aff3ad53b19fe8ffb982d6a509bc3c6685a30
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-18 14:05:03 +0200

    Create CustomAccountManager that is inherited from BaseUserManager

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+USER_TYPE_CHOICES = (
+    ('admin', 'admin'),
+    ('doctor', 'doctor'),
+    ('patient', 'patient'),
+    ('staff', 'staff'),
+    ('user_admin', 'user_admin'),
+)
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, username, first_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, username, first_name, password, **other_fields)
+
+    def create_user(self, email, username, first_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+        '''
+        One application of normalizing emails is to prevent multiple signups.
+         If your application lets the public to sign up,
+          your application might attract the "unkind" types
+        '''
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username,
+                          first_name=first_name, **other_fields)
+        #password must be stored using set_password() method
+        user.set_password(password)
+
+        user.save()
+        return user
+
+
+
