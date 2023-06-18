621d543d1b5b219180a55761a2779c1c231a9490
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-18 15:29:43 +0200

    register NewUser in admin.py file

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import *
+admin.site.register(NewUser)
+search_fields = ['email']
+autocomplete_fields = ['email', 'username', ]
