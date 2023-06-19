37502c3e181f977b033252aaa6723e511ffc3646
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-19 18:10:53 +0200

    Create Migrations for Scores Class

--- Scores/urls.py
+++ Scores/urls.py
@@ -4,6 +4,5 @@ from .views import *
 
 urlpatterns = [
 
-path('Scores/<int:session_id>',views.ScoresBySessionID,name='ScoresBySessionID'),
-path('Score',ScoreList.as_view(),name='score_list'),
 ]
+
