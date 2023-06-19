7374a539e9bd22bda29a29d00bb3268408b9b4e1
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-19 17:48:31 +0200

    Create new serializers.py

---
+++ Observation/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Observations
+class ObservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Observations
+        fields = "__all__"
