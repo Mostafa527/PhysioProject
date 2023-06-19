6981a8ca610d1b649de1868e742adf81326626b8
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-19 18:12:35 +0200

    Create urls.py

--- Scores/urls.py
+++ Scores/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
 
+path('Scores/<int:session_id>',views.ScoresBySessionID,name='ScoresBySessionID'),
+path('Score',ScoreList.as_view(),name='score_list'),
 ]
-
