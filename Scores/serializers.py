68cf511bf82e1a98a9698311df1b5cb6daa77cc7
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-06-19 18:12:18 +0200

    Create serializers class

---
+++ Scores/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import *
+
+class ScoreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model =Scores
+        fields = "__all__"
